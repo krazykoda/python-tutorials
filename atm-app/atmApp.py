bfe559a09d0f580cf2a0dbee489674db42c18dee
johnson <kryzbone@gmail.com>
2021-04-10 00:48:04 +0000

    improve atm app

---
+++ atm-app/atmApp.py
@@ -0,0 +1,145 @@
+from datetime import datetime
+import random
+
+database = {}
+
+current_user = None
+
+def init():
+	print("Welcome To My ATM")
+	print("1. Login \n2. Register \n3. Cancel")
+	print()
+
+	selection = int(input("Select option: "))
+
+	if(selection == 1):
+		login()
+	elif(selection == 2):
+		register()
+	elif(selection == 3):
+		print("See you soon, bye!")
+	else:
+		init()
+
+
+
+def login():
+	global current_user
+	email = input("Enter email: ")
+	password = input("Enter password: ")
+
+	user = database.get(email)
+
+	if(user and user['password'] == password):
+		current_user = user
+		print(f"Welcome {current_user['first_name']}")
+		print(f"Current time is {datetime.now().strftime('%c')}")
+		operations()
+	else:
+		print("Invalid email or password")
+		print()
+		init()
+
+
+
+def logout():
+	current_user = None
+	init()
+
+
+
+def register():
+	global current_user
+
+	email = input("Enter your email: ")
+	first_name = input("Enter your first name: ")
+	last_name = input("Enter your last name: ")
+	password = input("Enter your password: ")
+	acc_num = account_number()
+
+	database[email] = {
+		"first_name": first_name,
+		"last_name": last_name,
+		"password": password,
+		"account_number": acc_num,
+		"email": email,
+		"balance": 0
+	}
+
+	current_user = database[email]
+	print(f"Welcome {current_user['first_name']}")
+	print(f"Current time is {datetime.now().strftime('%c')}")
+	operations()
+
+
+def account_number():
+	actNum = random.randrange(1111111111,9999999999)
+	return actNum
+
+
+def withdraw():
+	amount = int(input("How much will you like to withdraw? "))
+	if(current_user['balance'] > amount):
+		current_user['balance'] -= amount 
+		database.update({current_user['email']: current_user})
+		print("Here is your cash")
+		print("current balance is %d" % current_user['balance'])
+		operations()
+	else:
+		print("Not enough balance")
+		operations()
+
+
+def deposit():
+	amount = int(input("How much will you like to deposit? "))
+	current_user['balance'] += amount 
+	database.update({current_user['email']: current_user})
+	print("current balance is %d" % current_user['balance'])
+	operations()
+
+
+def complaint():
+	complaint = input("What issue do you want to report? ")
+	print("Thanks for contacting us")
+	operations()
+
+
+def checkBalance():
+	print("current balance is %d" % current_user['balance'])
+	operations()
+
+
+
+def operations():
+	if(current_user):
+		print()
+		print("ATM operations")
+		print()
+		print("1. Withdraw")
+		print("2. Deposit")
+		print("3. Complaint")
+		print("4. Check Balance")
+		print("5. Logout")
+		print()
+
+		selection = int(input("What do you want to do? "))
+
+		if(selection == 1):
+			withdraw()
+		elif(selection == 2):
+			deposit()
+		elif(selection == 3):
+			complaint()
+		elif(selection == 4):
+			checkBalance()
+		elif(selection == 5):
+			logout()
+		else:
+			operations()
+	else:
+		init()
+
+
+
+# start App
+init()
